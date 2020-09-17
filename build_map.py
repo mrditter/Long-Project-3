b23512cd72281444629a03f24df97be4f17ad02b
mrditter <mrditter@gmail.com>
2020-09-16 18:02:48 -0700

    v1

--- build_map.py
+++ build_map.py
@@ -9,13 +9,39 @@ def build_map(infile) :
         if len(line) == 0 :
             continue
         line = line.split()
+        assert len(line) == 2
+        assert len(line[0]) == 1 and \
+            len(line[1]) == 1
+        assert line[0] not in key.keys()
         key[line[0]] = line[1]
-    print(key)
     return key
 
 def is_valid_map(key):
     "Checks the validity of the input file"
-    
+    for x, y in key.items() :
+        if type(x) != str:
+            return False
+        elif type(y) != str:
+            return False
+        elif len(x) != 1:
+            return False
+        elif len(y) != 1:
+            return False
+        elif x in " \t\n" :
+            return False
+        elif y in " \t\n" :
+            return False
+    dup_to = 0
+    for x in key.values():
+        dup_to = 0
+        for y in key.values():
+            if x == y:
+                dup_to += 1
+        if dup_to > 1:
+            return False
+    if len(key.items()) != len(key.keys()) :
+        return False
+    return True
 
 if __name__ == "__main__" :
     infile = "test1.txt"
