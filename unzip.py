4c46e6d177f551fbe21d097e9ad51c2e2284573e
mrditter <mrditter@gmail.com>
2020-09-17 17:50:59 -0700

    flawed logic fixed

--- unzip.py
+++ unzip.py
@@ -1,6 +1,8 @@
 def unzip(compressed):
     "'Unzips' the compressed stream"
     final_string = ""
+    current_index = 0
+    string_index = 0
     for i in compressed :
         assert (type(i) == str) or (type(i) == tuple)
         if type(i) == str :
@@ -8,21 +10,18 @@ def unzip(compressed):
         if type(i) == tuple:
             assert type(i[0]) == int
             assert type(i[1]) == int
-            assert type(i[0]) > 0
+            assert i[0] > 0
         if type(i) == str :
             final_string = final_string + i
-            continue
+            string_index += 1
         elif type(i) == tuple :
-            current_index, num_times = 0, 1
-            while id(compressed[current_index]) != id(i):
-                current_index += 1
-            end_index = current_index
-            current_index = current_index - i[0]
-            assert current_index >= 0
-            while num_times <= i[1] :
-                final_string = final_string + final_string[current_index]
-                if current_index + 1 != end_index :
-                    current_index += 1
+            num_times = 0
+            mini_index = string_index - i[0]
+            assert mini_index >= 0
+            while num_times < i[1] :
+                final_string = final_string + final_string[mini_index]
                 num_times += 1
+                mini_index += 1
+                string_index += 1
+        current_index += 1
     return final_string
-    
